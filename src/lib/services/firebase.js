e255c4dda254ab61667a89a218e74d5b0924b409
Noureldin Baraa Abdelhafez <xnourxb75@gmail.com>
2025-05-23 15:19:18 +0300

    ..

--- src/lib/services/firebase.js
+++ src/lib/services/firebase.js
@@ -1,17 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
+import { firebaseConfig } from "./firebaseconfig";
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    // TODO: Replace with your Firebase config object
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
